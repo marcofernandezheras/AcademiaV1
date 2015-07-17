9db5453f6db3dff569e9b346aba19fa11086e46c
marco <marcofernandezheras@gmail.com>
2015-07-17 14:02:30 +0200

    Created FileStudentManagerTest

--- AcademiaV1/src/tests/AllTests.java
+++ AcademiaV1/src/tests/AllTests.java
@@ -5,7 +5,10 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ FileObjectReaderTest.class, FileObjectWriterTest.class })
+@SuiteClasses({ 
+		FileObjectReaderTest.class, 
+		FileObjectWriterTest.class,
+		FileStudentManagerTest.class })
 public class AllTests {
 
 }
---
+++ AcademiaV1/src/tests/FileStudentManagerTest.java
@@ -0,0 +1,39 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FileStudentManagerTest {
+
+	@Test
+	public void testGetAllStudents() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStudentInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStudentString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateStudent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateStudent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteStudent() {
+		fail("Not yet implemented");
+	}
+
+}
