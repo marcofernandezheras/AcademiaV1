3b788c5e9db9b726ef4782010bb04551099b603a
marco <marcofernandezheras@gmail.com>
2015-07-17 07:38:10 +0200

    createStudents

--- AcademiaV1/src/model/StudentManager.java
+++ AcademiaV1/src/model/StudentManager.java
@@ -3,6 +3,7 @@ package model;
 public interface StudentManager {
 	public List<Student> getAllStudents();
 	public Student getStudent(int id);
+	public Student createStudent(String dni, String name, String surnames);
 	public void updateStudent(Student student);
 	public void deleteStudent(Student student);
 }
