fcb1945aa8197c669ce1441ecc5e039a88e1b948
marco <marcofernandezheras@gmail.com>
2015-07-17 12:17:57 +0200

    Created tests.FileObjectReaderTest

---
+++ AcademiaV1/src/tests/FileObjectReaderTest.java
@@ -0,0 +1,19 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FileObjectReaderTest {
+
+	@Test
+	public void testNextObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClose() {
+		fail("Not yet implemented");
+	}
+
+}
