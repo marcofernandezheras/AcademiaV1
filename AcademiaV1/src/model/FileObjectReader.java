71dc534c8a6504c95f4864344d5581dc79773ace
marco <marcofernandezheras@gmail.com>
2015-07-17 12:16:25 +0200

    Created model.FileObjectReader

---
+++ AcademiaV1/src/model/FileObjectReader.java
@@ -0,0 +1,19 @@
+package model;
+
+import java.io.EOFException;
+
+public class FileObjectReader implements ObjectReader {
+
+	@Override
+	public Object nextObject() throws EOFException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public void close() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
