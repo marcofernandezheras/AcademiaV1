20cd5ae8630f33f36f78a84849f106662d830555
marco <marcofernandezheras@gmail.com>
2015-07-17 12:10:59 +0200

    created Model.Student

---
+++ AcademiaV1/src/model/Student.java
@@ -0,0 +1,57 @@
+package model;
+
+import java.io.Serializable;
+
+@SuppressWarnings("serial")
+public class Student implements Serializable{
+	private int id;
+	private String dni;
+	private String name; 
+	private String surnames;
+	private String comments; 
+	
+	public Student(int id, String dni, String name, String surnames) {
+		super();
+		this.id = id;
+		this.dni = dni;
+		this.name = name;
+		this.surnames = surnames;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurnames() {
+		return surnames;
+	}
+
+	public void setSurnames(String surnames) {
+		this.surnames = surnames;
+	}
+
+	public String getComments() {
+		return comments;
+	}
+
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+
+	public int getId() {
+		return id;
+	}		
+	
+}
