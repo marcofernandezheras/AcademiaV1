08765d5c4cb3acca0d6083415c7fd0d75bb57ff9
marco <marcofernandezheras@gmail.com>
2015-07-20 15:45:08 +0200

    refactor Student to be child of Person

---
+++ AcademiaV1/src/model/Person.java
@@ -0,0 +1,59 @@
+package model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@SuppressWarnings("serial")
+public abstract class Person implements Serializable{
+	private int id;
+	private String dni;
+	private String name; 
+	private String surnames;
+	private Date bornDate;
+	
+	public Person(int id, String dni, String name, String surnames) {
+		super();
+		this.id = id;
+		this.dni = dni;
+		this.name = name;
+		this.surnames = surnames;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurnames() {
+		return surnames;
+	}
+
+	public void setSurnames(String surnames) {
+		this.surnames = surnames;
+	}
+
+	public Date getBornDate() {
+		return bornDate;
+	}
+
+	public void setBornDate(Date bornDate) {
+		this.bornDate = bornDate;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	
+}
--- AcademiaV1/src/model/Student.java
+++ AcademiaV1/src/model/Student.java
@@ -1,47 +1,12 @@
 package model;
 
-import java.io.Serializable;
-import java.util.Date;
-
 @SuppressWarnings("serial")
-public class Student implements Serializable{
-	private int id;
-	private String dni;
-	private String name; 
-	private String surnames;
+public class Student extends Person{
+	
 	private String comments; 
-	private Date bornDate;
 	
 	public Student(int id, String dni, String name, String surnames) {
-		super();
-		this.id = id;
-		this.dni = dni;
-		this.name = name;
-		this.surnames = surnames;
-	}
-
-	public String getDni() {
-		return dni;
-	}
-
-	public void setDni(String dni) {
-		this.dni = dni;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getSurnames() {
-		return surnames;
-	}
-
-	public void setSurnames(String surnames) {
-		this.surnames = surnames;
+		super(id, surnames, surnames, surnames);		
 	}
 
 	public String getComments() {
@@ -50,18 +15,6 @@ public class Student implements Serializable{
 
 	public void setComments(String comments) {
 		this.comments = comments;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public Date getBornDate() {
-		return bornDate;
-	}
-
-	public void setBornDate(Date bornDate) {
-		this.bornDate = bornDate;
-	}		
+	}	
 	
 }
