78c7a3025ca85fa6891b3645be70d216bbfa72f1
marco <marcofernandezheras@gmail.com>
2015-07-24 19:33:42 +0200

    dev FileGroupManager

--- AcademiaV1/src/controller/managers/FileGroupManager.java
+++ AcademiaV1/src/controller/managers/FileGroupManager.java
@@ -1,16 +1,23 @@
 package controller.managers;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import model.Group;
 import model.Student;
 import model.Teacher;
+import model.utils.Constants;
+import model.utils.FileObjectWriter;
+import model.utils.ObjectWriter;
 
 public class FileGroupManager implements GroupManager {
 
 	final private TeacherManager teacherManager;
 	final private StudentManager studentManager;
 	
+	private List<Group> groupList = new ArrayList<Group>();
+	private static int lastId = 1;
 	
 	public FileGroupManager(TeacherManager teacherManager,
 			StudentManager studentManager) {
@@ -20,16 +27,31 @@ public class FileGroupManager implements GroupManager {
 	}
 
 	@Override
-	public List<Group> getAllGroups() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Group> getAllGroups() {		
+		return groupList;
 	}
 
 	@Override
 	public Group createGroup(String groupName, Teacher teacher,
 			List<Student> students) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Integer> idList = new ArrayList<Integer>();
+		for (Student student : students) {
+			idList.add(student.getId());
+		}
+		Group group = new Group(lastId++, groupName, teacher.getId(), idList);
+		
+		try(ObjectWriter writer = new FileObjectWriter(Constants.GROUPS_FILE))
+		{
+			writer.writeObject(group);
+		} catch (IOException e) {			
+			e.printStackTrace();
+			return null;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+		return group;
 	}
 
 	@Override
